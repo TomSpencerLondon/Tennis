1c85942f9028fc53a26ce9257cba519193e1a9b0
Tom <thomas.spencer@codurance.com>
2020-07-02 19:05:08 +0100

    Add for draws

--- src/main/java/com/codurance/tennis/TennisScoreCalculator.java
+++ src/main/java/com/codurance/tennis/TennisScoreCalculator.java
@@ -3,9 +3,27 @@ package com.codurance.tennis;
 public class TennisScoreCalculator {
 
   public String score(int player1, int player2) {
-    if(player1 == 1 && player2 == 1){
-      return "Fifteen all";
+    StringBuilder stringBuilder = new StringBuilder();
+    for (Score score : Score.values()) {
+      if (player1 == score.input) {
+        stringBuilder.append(score.output);
+      }
     }
-    return "Love all";
+    return stringBuilder.append(" all").toString();
+  }
+
+  private enum Score {
+    LOVE(0, "Love"),
+    FIFTEEN(1, "Fifteen"),
+    THIRTY(2, "Thirty"),
+    FORTY(3, "Forty");
+
+    private final int input;
+    private final String output;
+
+    Score(int input, String output) {
+      this.input = input;
+      this.output = output;
+    }
   }
 }
--- src/test/java/com/codurance/tennis/TennisScoreCalculatorShould.java
+++ src/test/java/com/codurance/tennis/TennisScoreCalculatorShould.java
@@ -22,4 +22,14 @@ public class TennisScoreCalculatorShould {
   void return_fifteen_all_for_1_1() {
     assertEquals("Fifteen all", tennisScoreCalculator.score(1, 1));
   }
+
+  @Test
+  void return_thirty_all_for_2_2() {
+    assertEquals("Thirty all", tennisScoreCalculator.score(2, 2));
+  }
+
+  @Test
+  void return_forty_all_for_3_3() {
+    assertEquals("Forty all", tennisScoreCalculator.score(3, 3));
+  }
 }
