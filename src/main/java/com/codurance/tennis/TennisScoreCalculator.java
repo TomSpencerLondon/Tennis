f10660bb2705e3926463a2c711eec027b368680f
Tom <thomas.spencer@codurance.com>
2020-07-02 19:50:37 +0100

    Refactor

--- src/main/java/com/codurance/tennis/TennisScoreCalculator.java
+++ src/main/java/com/codurance/tennis/TennisScoreCalculator.java
@@ -21,33 +21,73 @@ public class TennisScoreCalculator {
 
   public String score(int player1Score, int player2Score) {
     StringBuilder result = new StringBuilder();
-    if(player1Score == player2Score && player1Score < 3){
-      result.append(scores.get(player1Score));
-      result.append(" all");
-    }else if(player1Score == player2Score){
-      result.append("Deuce");
-    }else if((player1Score >= 3 && player2Score >= 3) && (abs(player1Score - player2Score) == 1)){
-      if(player1Score > player2Score){
-        result.append("Advantage ");
-        result.append(player1);
-      }else{
-        result.append("Advantage " );
-        result.append(player2);
-      }
-    }else if((player1Score > 3 || player2Score > 3) && (abs(player1Score - player2Score) > 1)){
-      result.append("Game ");
-      if(player1Score > player2Score){
-        result.append(player1);
-      }else {
-        result.append(player2);
-      }
-    }else{
-      result.append(scores.get(player1Score));
-      result.append(", ");
-      result.append(scores.get(player2Score));
+    if(sameBelowForty(player1Score, player2Score)){
+      buildEvenString(player1Score, result);
+    }else if(differentBelowforty(player1Score, player2Score)){
+      buildScoreString(player1Score, player2Score, result);
+    }else if(isDeuce(player1Score, player2Score)){
+      buildDeuce(result);
+    }else if(isAdvantage(player1Score, player2Score)){
+      buildAdvantageString(player1Score, player2Score, result);
+    }else if(isWinner(player1Score, player2Score)){
+      buildWinnerString(player1Score, player2Score, result);
     }
 
     return result.toString();
+  }
+
+  private boolean sameBelowForty(int player1Score, int player2Score) {
+    return player1Score == player2Score && player1Score < 3;
+  }
+
+  private boolean differentBelowforty(int player1Score, int player2Score){
+    return player1Score != player2Score && player1Score < 3;
+  }
+
+  private void buildDeuce(StringBuilder result) {
+    result.append("Deuce");
+  }
+
+  private void buildEvenString(int player1Score, StringBuilder result) {
+    result.append(scores.get(player1Score));
+    result.append(" all");
+  }
+
+  private void buildScoreString(int player1Score, int player2Score, StringBuilder result) {
+    result.append(scores.get(player1Score));
+    result.append(", ");
+    result.append(scores.get(player2Score));
+  }
+
+  private void buildWinnerString(int player1Score, int player2Score, StringBuilder result) {
+    result.append("Game ");
+    if(player1Score > player2Score){
+      result.append(player1);
+    }else {
+      result.append(player2);
+    }
+  }
+
+  private void buildAdvantageString(int player1Score, int player2Score, StringBuilder result) {
+    if(player1Score > player2Score){
+      result.append("Advantage ");
+      result.append(player1);
+    }else{
+      result.append("Advantage " );
+      result.append(player2);
+    }
+  }
+
+  private boolean isDeuce(int player1Score, int player2Score) {
+    return player1Score == player2Score && player1Score >= 3;
+  }
+
+  private boolean isWinner(int player1Score, int player2Score) {
+    return (player1Score > 3 || player2Score > 3) && (abs(player1Score - player2Score) > 1);
+  }
+
+  private boolean isAdvantage(int player1Score, int player2Score) {
+    return (player1Score >= 3 && player2Score >= 3) && (abs(player1Score - player2Score) == 1);
   }
 
 }
